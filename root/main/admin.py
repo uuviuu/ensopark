342017c076f3d5a888333d33b8c8538c37361c42
agron <agronaut.nl@gmail.com>
2021-12-09 21:22:35 +0500

    News commit

--- root/main/admin.py
+++ root/main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import News
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date')
+
+
+
+admin.site.site_title = 'INIKOO'
+admin.site.site_header = 'INIKOO'
--- root/main/models.py
+++ root/main/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class News(models.Model):
+    """Новости на сайте"""
+    title = models.CharField('Заголовок', max_length=50, default='')
+    anons = models.CharField('Анонс', max_length=300, default='')
+    full_text = models.TextField('Новость', default='')
+    poster = models.ImageField('Постер', upload_to="News/", default='')
+    date = models.DateField('Дата', default=date.today)
+    
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        
+# class Prices(models.Model):
+    
+    
+#     def __str__(self):
+#         return self.status_name
+
+#     class Meta:
+#         verbose_name = 'Цена'
+#         verbose_name_plural = 'Цены'
+        
+# class Gallery(models.Model):
+    
+    
+#     def __str__(self):
+#         return self.status_name
+
+#     class Meta:
+#         verbose_name = 'Изображение'
+#         verbose_name_plural = 'Галерея'
+
+# class Contacts(models.Model):
+    
+    
+#     def __str__(self):
+#         return self.status_name
+
+#     class Meta:
+#         verbose_name = 'Контакт'
+#         verbose_name_plural = 'Контакты'
