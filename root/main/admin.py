b52da4ddb2a23aaf04e344b88762df22b01fd467
agron <agronaut.nl@gmail.com>
2021-12-11 00:08:13 +0500

    partner commit

--- root/main/admin.py
+++ root/main/admin.py
@@ -1,13 +1,85 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-from .models import News
+from .models import Gallery, News, Partners
 
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date')
+    list_display = ('title', 'date', 'get_img', 'url', 'draft')
+    list_display_links = ('title', )
+    list_filter = ('title', 'date',)
+    list_editable = ('draft',)
+    actions = ['publish', 'unpublish']    
+    fields = ('title', 'anons', 'text', 'date', 'image', 'get_img', 'url', 'draft')
+    readonly_fields = ('get_img', )
+    search_fields = ('title', )
+    save_on_top = True
+    save_as = True 
+        
+    def get_img(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="88px">')
+        else:
+            return 'нет картинки'
+    
+    def unpublish(self, request, queryset):
+        """Снять с публикации"""
+        row_update = queryset.update(draft=True)
+        if row_update == 1:
+            message_bit = '1 запись была обновлена'
+        else:
+            message_bit = f'{ row_update } записи были обновлены'
+        self.message_user(request, f'{ message_bit }') 
+        
+    def publish(self, request, queryset):
+        """Опубликовать"""
+        row_update = queryset.update(draft=False)
+        if row_update == 1:
+            message_bit = '1 запись была обновлена'
+        else:
+            message_bit = f'{ row_update } записи были обновлены'
+        self.message_user(request, f'{ message_bit }')    
+        
+    publish.short_description = 'Опубликовать'
+    publish.allowed_permissions = ('change', )    
+    
+    unpublish.short_description = 'Снять с публикации'
+    unpublish.allowed_permissions = ('change', )  
+    
+    get_img.short_description = 'Миниатюра'
 
-
-
-admin.site.site_title = 'INIKOO'
-admin.site.site_header = 'INIKOO'
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'season', 'date', 'get_img')
+    list_display_links = ('title', 'get_img')
+    list_filter = ('title', 'season', 'date',)  
+    search_fields = ('title', 'season')
+    fields = ('title', 'season', 'date', 'image', 'get_img')
+    readonly_fields = ('get_img', )
+    
+    def get_img(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="88px">')
+        else:
+            return 'нет картинки'
+        
+    get_img.short_description = 'Миниатюра'
+    
+@admin.register(Partners)
+class PartnersAdmin(admin.ModelAdmin):
+    list_display = ('partner', 'get_img')
+    list_display_links = ('partner', 'get_img')
+    fields = ('partner', 'image', 'get_img')
+    readonly_fields = ('get_img', )
+    
+    def get_img(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="88px">')
+        else:
+            return 'нет картинки'
+        
+    get_img.short_description = 'Миниатюра'
+    
+admin.site.site_title = 'ENSOPARK'
+admin.site.site_header = 'ENSOPARK'
--- root/main/models.py
+++ root/main/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 from datetime import date
 
 
@@ -6,19 +8,68 @@ class News(models.Model):
     """Новости на сайте"""
     title = models.CharField('Заголовок', max_length=50, default='')
     anons = models.CharField('Анонс', max_length=300, default='')
-    full_text = models.TextField('Новость', default='')
-    poster = models.ImageField('Постер', upload_to="News/", default='')
+    text = models.TextField('Новость', default='')
+    image = models.ImageField("Изображение", upload_to="news/", default='')
     date = models.DateField('Дата', default=date.today)
+    url = models.SlugField(max_length=130, unique=True, default='')
+    draft = models.BooleanField("Черновик", default=False)
     
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("news", kwargs={"slug": self.url})
+
+    def get_review(self):
+        return self.reviews_set.filter(parent__isnull=True)
 
     class Meta:
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
         
-# class Prices(models.Model):
+class Gallery(models.Model):
+    """Галерея"""
+    title = models.CharField("Заголовок", max_length=50, default='')
+    season = models.CharField("Сезон", max_length=50, default='')
+    image = models.ImageField("Изображение", upload_to="gallery/")
+    date = models.DateField('Дата', default=date.today)
     
+    def __str__(self):
+        return self.title 
+
+    class Meta:
+        verbose_name = 'Изображение'
+        verbose_name_plural = 'Галерея'
+
+class Partners(models.Model):
+    """Партнеры"""
+    partner = models.CharField("Партнер", max_length=100)
+    image = models.ImageField("Изображение", upload_to="partners/")
+    
+    def __str__(self):
+        return self.partner
+
+    class Meta:
+        verbose_name = 'Партнер'
+        verbose_name_plural = 'Партнеры'   
+       
+# class Contacts(models.Model):
+#     """Контактная информация"""
+#     email = models.EmailField('Email', max_length=100)
+#     number = models.CharField("Номер", max_length=12)
+#     address = models.CharField("Адрес", max_length=100)
+#     description = models.TextField("Описание")
+    
+#     def __str__(self):
+#         return self.email
+
+#     class Meta:
+#         verbose_name = 'Контакт'
+#         verbose_name_plural = 'Контакты'      
+
+
+        
+# class Prices(models.Model):
     
 #     def __str__(self):
 #         return self.status_name
@@ -26,23 +77,4 @@ class News(models.Model):
 #     class Meta:
 #         verbose_name = 'Цена'
 #         verbose_name_plural = 'Цены'
-        
-# class Gallery(models.Model):
-    
-    
-#     def __str__(self):
-#         return self.status_name
 
-#     class Meta:
-#         verbose_name = 'Изображение'
-#         verbose_name_plural = 'Галерея'
-
-# class Contacts(models.Model):
-    
-    
-#     def __str__(self):
-#         return self.status_name
-
-#     class Meta:
-#         verbose_name = 'Контакт'
-#         verbose_name_plural = 'Контакты'
